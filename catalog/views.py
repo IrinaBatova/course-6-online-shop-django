4d69042bc1df6575acdc471407a99e9e4b3f530c
Irina Batova <BatIr_74_2@mail.ru>
2026-08-21 20:08:54 +0300

    A contact form has been implemented and a README file has been added.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """Контроллер для отображения домашней страницы."""
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    """Контроллер для отображения страницы контактов и обработки формы."""
+    context = {}
+
+    if request.method == 'POST':
+        # Получаем данные из оригинальной HTML-формы курса
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')  # Считываем переданный телефон
+        message = request.POST.get('message')
+
+        # Выводим полученные данные в консоль PyCharm
+        print(f"Новое обращение!\nИмя: {name}\nТелефон: {phone}\nСообщение: {message}\n" + "-" * 20)
+
+        # Передаем флаг успешной отправки для отображения плашки alert
+        context['success'] = True
+
+    return render(request, 'catalog/contacts.html', context)
